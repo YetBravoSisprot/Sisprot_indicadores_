1bdee52e7fb1afd2805c3b17aab875a0232d1efd
YetBravoSisprot <ybravo@sisprotgf.com>
2026-01-18 20:54:22 -0400

    Refactor LogoTitulo component with default variant
    Refactor LogoTitulo component to set default variant and improve structure.

--- src/Componentes/LogoTitulo.jsx
+++ src/Componentes/LogoTitulo.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./LogoTitulo.css";
 
-const LogoTitulo = ({ variant }) => {
+const LogoTitulo = ({ variant = "default" }) => {
   return (
-    <div className={`logo-wrapper ${variant === "login" ? "login" : ""}`}>
-      <img src="/logo_sgf.png" alt="Sisprot Global Fiber" />
-      <h1>Sisprot Global Fiber</h1>
-      <span>App</span>
+    <div className={`logo-wrapper ${variant}`}>
+      <img
+        src="/logo_sgf.png"
+        alt="Sisprot Global Fiber"
+        className="logo-img"
+      />
+
+      <div className="logo-text">
+        <h1>Sisprot Global Fiber</h1>
+        <span>App</span>
+      </div>
     </div>
   );
 };
