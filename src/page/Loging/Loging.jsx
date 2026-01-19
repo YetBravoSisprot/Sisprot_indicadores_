f5dd4ef244ce1e9ed4d2467e9191e537d33dc796
YetBravoSisprot <ybravo@sisprotgf.com>
2026-01-18 20:12:22 -0400

    Fix typo in authentication error message

--- src/page/Loging/Loging.jsx
+++ src/page/Loging/Loging.jsx
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
-
 import LogingForm from "../../Componentes/LogingForm";
 import PageNav from "../../Componentes/PageNav";
 import LogoTitulo from "../../Componentes/LogoTitulo";
 import { PasswordContext } from "../../PasswordContext/PasswordContext";
 import DropdownMenu from "../../Componentes/DropdownMenu";
+import "./login.css";
 
 function Loging() {
   const { showPasswordState, isAuthenticated, role } =
     useContext(PasswordContext);
 
-  // Usuario autenticado pero sin permisos
+  // Usuario autenticado sin permisos
   if (!showPasswordState && isAuthenticated && role !== "admin" && role !== "ventas") {
     return (
-      <div className="login-container">
-        <div className="login-card">
+      <div className="login-wrapper">
+        <div className="login-animated-card shake">
           <LogoTitulo />
-          <p className="login-error">No autorizado</p>
+          <p className="login-error">Acceso no autorizado</p>
           <DropdownMenu />
           <LogingForm />
         </div>
@@ -25,9 +25,9 @@ function Loging() {
   }
 
   return (
-    <div className="login-container">
+    <div className="login-wrapper">
       {showPasswordState ? (
-        <div className="login-card">
+        <div className="login-animated-card fade-slide">
           <LogoTitulo />
           <LogingForm />
         </div>
