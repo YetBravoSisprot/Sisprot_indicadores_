ded7c12a9946e824639e02b6a99bb3110938c82e
YetBravoSisprot <ybravo@sisprotgf.com>
2026-02-02 01:12:49 -0400

    Update PageNotFound.jsx

--- src/page/PageNotFound/PageNotFound.jsx
+++ src/page/PageNotFound/PageNotFound.jsx
@@ -25,7 +25,7 @@ function PageNotFound() {
           title="Indicadores con API"
           width="100%" 
           height="600" 
-          src="https://app.powerbi.com/reportEmbed?reportId=7e652285-60ce-4184-bf73-e9c43319f894&autoAuth=true&experience=power-bi"
+          src="https://app.powerbi.com/reportEmbed?reportId=f294a5a4-0a10-43c8-b212-64521a9e66f1&autoAuth=true&experience=power-bi"
           frameBorder="0" 
           allowFullScreen={true}
           style={{marginBottom: '20px'}}
