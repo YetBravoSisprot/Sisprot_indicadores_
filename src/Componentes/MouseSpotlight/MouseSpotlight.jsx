272c88ee3a01b07bbe9497d1be2bdb7498d46594
YetBravoSisprot <ybravo@sisprotgf.com>
2026-02-08 20:56:27 -0400

    Create MouseSpotlight.jsx

---
+++ src/Componentes/MouseSpotlight/MouseSpotlight.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import './MouseSpotlight.css';
+
+const MouseSpotlight = () => {
+  const spotlightRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (spotlightRef.current) {
+        spotlightRef.current.style.setProperty('--mouse-x', `${e.clientX}px`);
+        spotlightRef.current.style.setProperty('--mouse-y', `${e.clientY}px`);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={spotlightRef}
+      className="mouse-spotlight"
+    />
+  );
+};
+
+export default MouseSpotlight;
