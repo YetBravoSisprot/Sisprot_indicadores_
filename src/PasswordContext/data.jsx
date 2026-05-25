e3acae99a11c32850dd0b555625ff6402d992d27
Yet-bravo <yet.brm@gmail.com>
2026-05-25 12:21:36 -0400

    Actualización de archivo desde N8N

--- src/PasswordContext/data.jsx
+++ src/PasswordContext/data.jsx
@@ -103848,11 +103848,11 @@ const largeArraydata =
       "migrate": false,
       "sector_name": "Prados III",
       "plan": {
-        "id": 21,
-        "name": "RECURRENTE RESIDENCIAL PLAN 450M",
-        "cost": "34.50",
-        "code": "FOR450M",
-        "profile": "450M/450M"
+        "id": 100,
+        "name": "RECURRENTE RESIDENCIAL PLAN 300M",
+        "cost": "27.60",
+        "code": "FOR300M",
+        "profile": "300Mbps/300Mbps"
       },
       "client_mobile": "04243120052",
       "address": "URB.PRADO III CALLE 2 NRO 28",
@@ -109185,11 +109185,11 @@ const largeArraydata =
       "migrate": true,
       "sector_name": "Prados de Cafetal",
       "plan": {
-        "id": 21,
-        "name": "RECURRENTE RESIDENCIAL PLAN 450M",
-        "cost": "34.50",
-        "code": "FOR450M",
-        "profile": "450M/450M"
+        "id": 100,
+        "name": "RECURRENTE RESIDENCIAL PLAN 300M",
+        "cost": "27.60",
+        "code": "FOR300M",
+        "profile": "300Mbps/300Mbps"
       },
       "client_mobile": "04243769743",
       "address": "PRADOS DEL CAFETAL CALLE 1 AL FINAL DE LA CALLE",
