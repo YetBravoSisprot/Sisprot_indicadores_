b6503af35bf4193e26bf93d1de6d342ca834b9de
YetBravoSisprot <ybravo@sisprotgf.com>
2026-07-17 17:49:26 -0400

    Update DropdownMenu.jsx

--- src/Componentes/DropdownMenu.jsx
+++ src/Componentes/DropdownMenu.jsx
@@ -1,37 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./DropdownMenu.css";
 import { NavLink } from "react-router-dom";
+import { PasswordContext } from "../PasswordContext/PasswordContext";
 
 function DropdownMenu() {
+  const { role } = useContext(PasswordContext);
+
+  const links = [
+    { to: "/TopUrbanismo", label: "Top Urbanismos", roles: ["admin"] },
+    { to: "/*", label: "Reportes BI", roles: ["admin"] },
+    { to: "/Indicadores", label: "Indicadores", roles: ["admin"] },
+    { to: "/Ventas", label: "Operaciones", roles: ["admin", "ventas"] },
+    { to: "/Admin", label: "Adm. Ingresos", roles: ["admin"] },
+    { to: "/Reactivados", label: "Control Reactivados", roles: ["admin", "analista_atencion"] },
+    { to: "/Encuestas", label: "Encuestas", roles: ["admin", "analista_atencion"] },
+  ];
+
+  const filteredLinks = links.filter((link) => !link.roles || link.roles.includes(role));
+
   return (
     <div className="dropdown-container">
       {/* Eliminamos el botón de menú ya que no lo necesitamos */}
       <ul className="dropdown-content">
-        <li>
-          <NavLink to="/TopUrbanismo" className="dropdown-link">
-            Top Urbanismos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/*" className="dropdown-link">
-            Reportes BI
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Indicadores" className="dropdown-link">
-            Indicadores
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Ventas" className="dropdown-link">
-            Operaciones
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Admin" className="dropdown-link">
-            Adm. Ingresos
-          </NavLink>
-        </li>
+        {filteredLinks.map((link) => (
+          <li key={link.to}>
+            <NavLink to={link.to} className="dropdown-link">
+              {link.label}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </div>
   );
