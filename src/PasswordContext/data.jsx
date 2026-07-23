9785da3d4d4c798cec257460e27a980c572a831b
Yet-bravo <yet.brm@gmail.com>
2026-07-23 16:47:33 -0400

    Actualización de archivo desde N8N

--- src/PasswordContext/data.jsx
+++ src/PasswordContext/data.jsx
@@ -121515,7 +121515,7 @@ const largeArraydata =
       "client_subdivision": "SUSPENDIDO_PYME",
       "status_name": "Suspendido",
       "cycle": 1,
-      "migrate": false,
+      "migrate": true,
       "sector_name": "Sabana Grande",
       "plan": {
         "id": 34,
