45bc022a264e1b022fe122236ddecd55691d40f0
Yet-bravo <yet.brm@gmail.com>
2026-09-09 14:18:02 -0400

    Actualización de archivo desde N8N

--- src/PasswordContext/data.jsx
+++ src/PasswordContext/data.jsx
@@ -104657,7 +104657,7 @@ const largeArraydata =
       "client_subdivision": "ACTIVO_RESIDENCIAL",
       "status_name": "Activo",
       "cycle": 1,
-      "migrate": false,
+      "migrate": true,
       "sector_name": "Casco de Turmero",
       "plan": {
         "id": 100,
