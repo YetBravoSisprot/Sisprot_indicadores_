d1a89a666fb6333c2cff79bf083dae0951e2e785
Yet-bravo <yet.brm@gmail.com>
2026-09-21 23:18:57 -0400

    Actualización de archivo desde N8N

--- src/PasswordContext/data.jsx
+++ src/PasswordContext/data.jsx
@@ -167188,7 +167188,7 @@ const largeArraydata =
       "client_subdivision": "ACTIVO_RESIDENCIAL",
       "status_name": "Activo",
       "cycle": 1,
-      "migrate": false,
+      "migrate": true,
       "sector_name": "El Macaro",
       "plan": {
         "id": 100,
